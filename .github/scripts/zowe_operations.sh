ff90a349e19168f06b3b1c81aa9c42caedf429fb
Arish Kumar Dhara <64302065+iamakd10@users.noreply.github.com>
2026-02-20 19:32:12 +0530

    Enhance Zowe operations script for uploads
    Updated the Zowe operations script to include recursive upload and specify binary files.

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,20 +1,11 @@
-echo "Initializing Zowe configuration..."
-zowe config init --global-config
-echo "Setting Zowe connection properties..."
-zowe config set profiles.zosmf.properties.host $ZOWE_HOST 
-zowe config set profiles.zosmf.properties.port 10443
-zowe config set profiles.zosmf.properties.user $ZOWE_USERNAME 
-zowe config set profiles.zosmf.properties.password $ZOWE_PASSWORD 
-zowe config set profiles.zosmf.properties.rejectUnauthorized false
-echo "Zowe config successfully!"
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" > /dev/null 2>&1 then
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &> /dev/null; then
   echo "Directory does not exist. Creating it..."
   zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
 else
   echo "Directory already exists!"
 fi
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
 echo "Verifying upload..."
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
 echo "Uploaded successfully!"
